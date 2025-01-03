6bdabcfc4e3a7ea54d009b0021f59676b7f6c769
Md Nehal Ahmad <nehal.ahmad@paynways.com>
2025-01-03 18:42:43 +0530

    chnages

---
+++ Day2/DataTypeConversion.js
@@ -0,0 +1,28 @@
+// console.log("Hello World");
+
+// let num = 565;
+// console.log(typeof num);
+// console.log(typeof String(num));
+// console.log(typeof (num).toString() );
+// console.log(typeof num);
+
+// console.log(parseInt(77.88));
+// console.log(parseFloat(767))
+
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+// console.log(Boolean(2));
+// console.log(Boolean(3));
+console.log(123 * 2)
+console.log("123" * 2);
+
+
+console.log(100 / 5);
+console.log("100" / 5);
+console.log("------------------------")
+console.log(100 == 100);
+console.log("100" == 100);
+console.log("100" === 100);
+
+console.log(100+100);
+console.log("100" + 100);
---
+++ Day2/DataTypeConversionExample.js
@@ -0,0 +1,27 @@
+console.log("Hello");
+
+
+console.log(100 * 2);
+console.log("100" * 2);
+
+console.log(90 + 2);
+console.log("90" + 2);
+console.log("90"+"2");
+
+
+console.log(100-2);
+console.log("100"-2);
+console.log("--------------------------")
+console.log("35"-2);
+console.log("35"-0);
+console.log("abd66"-12);
+
+
+let num = 123;
+let StrNum = String(num);
+console.log(StrNum);
+
+console.log(String(7474));
+console.log(Number("abc"));
+console.log(Number("123"));
+console.log(Number("abc34"))
