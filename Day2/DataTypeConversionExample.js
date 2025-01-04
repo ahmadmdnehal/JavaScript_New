48d4a7c46761f7484b000c06eba085c40a1de60a
Md Nehal Ahmad <nehal.ahmad@paynways.com>
2025-01-04 12:19:18 +0530

    changes

--- Day2/DataTypeConversionExample.js
+++ Day2/DataTypeConversionExample.js
@@ -1,27 +1,32 @@
-// console.log("Hello");
+console.log("Hello");
 
 
-// console.log(100 * 2);
-// console.log("100" * 2);
+console.log(100 * 2);
+console.log("100" * 2);
 
-// console.log(90 + 2);
-// console.log("90" + 2);
-// console.log("90"+"2");
-
-
-// console.log(100-2);
-// console.log("100"-2);
-// console.log("--------------------------")
-// console.log("35"-2);
-// console.log("35"-0);
-// console.log("abd66"-12);
+console.log(90 + 2);
+console.log("90" + 2);
+console.log("90"+"2");
 
 
-// let num = 123;
-// let StrNum = String(num);
-// console.log(StrNum);
+console.log(100-2);
+console.log("100"-2);
+console.log("--------------------------")
+console.log("35"-2);
+console.log("35"-0);
+console.log("abd66"-12);
 
-// console.log(String(7474));
-// console.log(Number("abc"));
-// console.log(Number("123"));
-// console.log(Number("abc34"))
+
+let num = 123;
+let StrNum = String(num);
+console.log(StrNum);
+
+console.log(String(7474));
+console.log(Number("abc"));
+console.log(Number("123"));
+console.log(Number("abc34"))
+
+
+console.log(parseInt("gnd"));
+console.log(parseFloat("5353.ksj"));
+console.log(parseFloat("jdndn.888"));
